20914fa6b02bfd335bb46587e1f389c773a0e1e3
Felix Larsson <felixlarssoonn@MacBook-Air-som-tillhor-Felix.local>
2026-03-24 01:47:01 +0100

    cors fix

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const puppeteer = require("puppeteer");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 const PORT = process.env.PORT || 3000;
 
 app.get("/search", async (req, res) => {
